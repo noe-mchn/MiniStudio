befb71c52e5a043c10286ac130b8ca42df250c87
ValentineFlo <vflorens@gaming.tech>
2025-03-20 23:02:02 +0100

    AnimEtTuiles

--- Game/src/map.cpp
+++ Game/src/map.cpp
@@ -8,7 +8,7 @@ TilesMap::TilesMap(const MapRepresentation& repr, TextureCache* texture) : m_wid
 {
     m_width = repr[0].size();
     m_height = repr.size();
-    m_tileSize = 32;
+    m_tileSize = 800;
 
     m_tiles.resize(m_width * m_height);
 
@@ -26,7 +26,7 @@ TilesMap::TilesMap(const MapRepresentation& repr, TextureCache* texture) : m_wid
 void TilesMap::render(sf::RenderWindow& w)
 {
 
-    sf::Texture& texture = m_texture->getTexture("preview_774.png");
+    sf::Texture& texture = m_texture->getTexture("galaxie3.png");
 
     for (int row = 0; row < m_height; ++row)
     {
